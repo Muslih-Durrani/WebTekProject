ea3eb004639461f8782737078150fe58c07afd2e
Muslih-Durrani <162908563+Muslih-Durrani@users.noreply.github.com>
2025-05-10 18:09:08 +0300

    index.php
    Home Page

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
+
+<main class="container my-5">
+    <section class="row">
+        <div class="col-md-6">
+            <h1 class="display-4">About Me</h1>
+            <p class="lead">Hello! I'm Muslih Durrani, Founder-CEO at Axoré | Computer Engineering student at Sakarya University.</p>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h2>Hobbies & Interests</h2>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Programming</li>
+                        <li class="list-group-item">Photography</li>
+                        <li class="list-group-item">Hiking</li>
+                        <li class="list-group-item">Reading</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <img src="assets/images/profile.jpg" alt="Profile Picture" class="img-fluid rounded-circle  w-50">
+
+            <div class="card mt-4">
+                <div class="card-body">
+                    <h3>Favorite Sports</h3>
+                    <div class="d-flex justify-content-between">
+                        <div class="text-center">
+                            <img src="https://editor.hibya.com/upload/1704101740.jpg" alt="F1" class="img-thumbnail">
+                            <p>F1</p>
+                        </div>
+                        <div class="text-center">
+                            <img src="https://www.sportphotogallery.com/content/images/cmsfiles/product/33993/34733-list.jpg" alt="Cricket" class="img-thumbnail">
+                            <p>Cricket</p>
+                        </div>
+                
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
